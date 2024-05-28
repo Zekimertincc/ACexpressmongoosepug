a0d7dfceef7c5b74878ff13f4002042681e21dcc
Zekimertincc <Zekimertinc@gmail.com>
2024-05-28 02:12:30 +0200

    stylesheet problemli

--- Acdeneme/routes/index.js
+++ Acdeneme/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
+// Ana sayfa
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
+
+// Diğer sayfalar
+router.get('/about', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views', 'about.html'));
 });
 
 module.exports = router;
